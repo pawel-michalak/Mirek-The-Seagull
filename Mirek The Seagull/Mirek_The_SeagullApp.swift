cdba11b43a986b4b5257fe0e8c19defa8d90aebc
Pawel Michalak <pawelmichalak@icloud.com>
2021-03-12 11:19:16 +0100

    Initial Commit

---
+++ Mirek The Seagull/Mirek_The_SeagullApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mirek_The_SeagullApp.swift
+//  Mirek The Seagull
+//
+//  Created by Paweł Michalak on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Mirek_The_SeagullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
